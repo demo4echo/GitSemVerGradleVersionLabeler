7fe26dda6b0995aeba86fce9545be67ec02ca345
Tiran Meltser <tiran.meltser@efrat.com>
2020-04-25 01:07:58 +0300

    Update

--- bin/invokeGradle.sh
+++ bin/invokeGradle.sh
@@ -6,10 +6,16 @@
 # will cause all the git related operations (mainly tagging) to be performed on the sub module itself instead on the main (containing repo).
 #
 
+# Keep current location
+ORIGINAL_PATH=$PWD
+
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
-#SCRIPT=$(readlink -f "$0")
+SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
-#SCRIPT_PATH=$(dirname "$SCRIPT")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+# Move to this script location
+cd "$SCRIPT_PATH"
 
 # Find the repo folder name
 cd ..
@@ -19,11 +25,13 @@ cd -
 # Generate a UUID and target folder name to copy this repo to
 REPO_COPY_DESIGNATED_FOLDER_NAME=`uuidgen`
 
-#cd "$SCRIPT_PATH"
+# Copt the repo and invoke the gradle wrapper from there
 cd ../..
 mkdir -p $REPO_COPY_DESIGNATED_FOLDER_NAME && rsync $REPO_FOLDER_NAME $REPO_COPY_DESIGNATED_FOLDER_NAME --exclude .git/ --exclude .gradle/ --exclude bin/ --exclude .gitattributes --exclude .gitignore
 cd $REPO_COPY_DESIGNATED_FOLDER_NAME
 ./gradlew $*
 cd -
 rm -rf ../../$REPO_COPY_DESIGNATED_FOLDER_NAME
-# cd -
+
+# Return to the original location
+cd $ORIGINAL_PATH
